1601393efdf2b07ab905d614467bc3054b8d23a4
Abhay singh <abhaysingh936851@gmail.com>
2022-04-02 09:02:01 +0530

    Implemented recycler view for worker activities

---
+++ app/src/main/java/com/example/servo/Adapter/WorkerPendingActivityAdapter.java
@@ -0,0 +1,75 @@
+package com.example.servo.Adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.servo.R;
+import com.example.servo.StudentPendingInfo;
+import com.example.servo.WorkerPendingActivity;
+import com.example.servo.WorkerPendingInfo;
+
+import java.util.ArrayList;
+
+public class WorkerPendingActivityAdapter extends RecyclerView.Adapter<WorkerPendingActivityAdapter.WorkerPendingActivityHolder> {
+
+
+    private Context context;
+    private ArrayList<WorkerPendingInfo> workerPendingInfos;
+
+    public WorkerPendingActivityAdapter(Context context, ArrayList<WorkerPendingInfo> workerPendingInfos) {
+        this.context = context;
+        this.workerPendingInfos = workerPendingInfos;
+    }
+
+    @NonNull
+    @Override
+    public WorkerPendingActivityHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.pending_worker_card_layout, parent, false);
+        return new WorkerPendingActivityHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WorkerPendingActivityHolder holder, int position) {
+        WorkerPendingInfo workerPendingInfo = workerPendingInfos.get(position);
+        holder.SetDetails(workerPendingInfo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return workerPendingInfos.size();
+    }
+
+
+    public class WorkerPendingActivityHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtWIdNo, txtWType, txtWDate, txtWTime, txtWRoom, txtWRoll, txtWContact;
+        public WorkerPendingActivityHolder(@NonNull View itemView) {
+            super(itemView);
+            txtWIdNo = itemView.findViewById(R.id.pwIdNo);
+            txtWType = itemView.findViewById(R.id.pwType);
+            txtWDate = itemView.findViewById(R.id.pwDate);
+            txtWTime = itemView.findViewById(R.id.pwTime);
+            txtWRoom = itemView.findViewById(R.id.pwRoomNo);
+            txtWRoll = itemView.findViewById(R.id.pwRollNo);
+            txtWContact = itemView.findViewById(R.id.pwContactNo);
+        }
+
+        @SuppressLint("SetTextI18n")
+        void SetDetails(WorkerPendingInfo workerPendingInfo) {
+            txtWIdNo.setText("NO : " + workerPendingInfo.getIdNo());
+            txtWType.setText("TYPE : " + workerPendingInfo.getType());
+            txtWRoom.setText("ROOM NO : " + workerPendingInfo.getRoomNO());
+            txtWRoll.setText("ROLL NO : " + workerPendingInfo.getRollNO());
+            txtWContact.setText("CONTACT NO : " + workerPendingInfo.getContactNo());
+            txtWDate.setText(workerPendingInfo.getDate());
+            txtWTime.setText(workerPendingInfo.getTime());
+        }
+    }
+}
--- app/src/main/java/com/example/servo/WorkerActivity.java
+++ app/src/main/java/com/example/servo/WorkerActivity.java
@@ -1,42 +1,92 @@
 package com.example.servo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.servo.Adapter.CompletedActivityAdapter;
+import com.example.servo.Adapter.StudentPendingActivityAdapter;
+import com.example.servo.Adapter.WorkerPendingActivityAdapter;
+
+import java.util.ArrayList;
+
 public class WorkerActivity extends AppCompatActivity {
 
-    Button pendingWork;
-    Button completedWork;
+    RecyclerView workerPendingActivityRecyclerView;
+    RecyclerView completedActivityRecyclerView;
+    private CompletedActivityAdapter completedActivityAdapter;
+    private ArrayList<CompletedActivityInfo> completedActivityInfos;
+    private WorkerPendingActivityAdapter workerPendingActivityAdapter;
+    private ArrayList<WorkerPendingInfo> workerPendingInfos;
+    Button pendingWorker;
+    Button completedWorker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker);
 
-        pendingWork = findViewById(R.id.workerPendingBtn);
-        completedWork = findViewById(R.id.workerCompletedBtn);
+        pendingWorker = findViewById(R.id.pendingBtnWorker);
+        completedWorker = findViewById(R.id.completedBtnWorker);
+        workerPendingActivityRecyclerView = findViewById(R.id.workerPendingRecyclerView);
+        completedActivityRecyclerView = findViewById(R.id.workerCompletedRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        workerPendingActivityRecyclerView.setLayoutManager(linearLayoutManager);
+        workerPendingInfos = new ArrayList<WorkerPendingInfo>();
+        workerPendingActivityAdapter = new WorkerPendingActivityAdapter(this, workerPendingInfos);
+        workerPendingActivityRecyclerView.setAdapter(workerPendingActivityAdapter);
+        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        completedActivityRecyclerView.setLayoutManager(linearLayoutManager1);
+        completedActivityInfos = new ArrayList<CompletedActivityInfo>();
+        completedActivityAdapter = new CompletedActivityAdapter(this, completedActivityInfos);
+        completedActivityRecyclerView.setAdapter(completedActivityAdapter);
 
-        pendingWork.setOnClickListener(new View.OnClickListener() {
+        createCompletedListData();
+        createStudentPendingListData();
+        pendingWorker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(WorkerActivity.this, WorkerPendingActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-
+                pendingWorker.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+                completedWorker.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.white)));
+                pendingWorker.setTextColor(getColor(R.color.white));
+                completedWorker.setTextColor(getColor(R.color.colorPrimary));
+                workerPendingActivityRecyclerView.setVisibility(View.VISIBLE);
+                completedActivityRecyclerView.setVisibility(View.INVISIBLE);
             }
         });
 
-        completedWork.setOnClickListener(new View.OnClickListener() {
+        completedWorker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(WorkerActivity.this, CompletedActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-
+                completedWorker.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+                pendingWorker.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.white)));
+                completedWorker.setTextColor(getColor(R.color.white));
+                pendingWorker.setTextColor(getColor(R.color.colorPrimary));
+                workerPendingActivityRecyclerView.setVisibility(View.INVISIBLE);
+                completedActivityRecyclerView.setVisibility(View.VISIBLE);
             }
         });
 
 
 
+    }
+
+    private void createCompletedListData() {
+        CompletedActivityInfo completedActivityInfo = new CompletedActivityInfo("4827890", "casfdwadf", "1/4/2022", "5:10 PM", "1/4/2022", "5:10 PM");
+        CompletedActivityInfo completedActivityInfo1 = new CompletedActivityInfo("4827890", "casfdwadf", "1/4/2022", "5:10 PM", "1/4/2022", "5:10 PM");
+        completedActivityInfos.add(completedActivityInfo);
+        completedActivityInfos.add(completedActivityInfo1);
+    }
+
+    private void createStudentPendingListData() {
+        WorkerPendingInfo workerPendingInfo = new WorkerPendingInfo("1234", "ELECTRICIAN", "1/4/2022", "5:10 PM", "5607", "IEC2020080", "9761319703");
+        WorkerPendingInfo workerPendingInfo1 = new WorkerPendingInfo("1234", "ELECTRICIAN", "1/4/2022", "5:10 PM", "5607", "IEC2020080", "9761319703");
+        workerPendingInfos.add(workerPendingInfo);
+        workerPendingInfos.add(workerPendingInfo1);
     }
 }
